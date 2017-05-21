b7328733b5dd3cbb386a209f08b2bf97b2f65077
Kenneth <Kenneth@Knights.UCF.edu>
2017-05-21 17:26:22 -0400

    Fixed whitespace inconsistencies in Arduino code

--- Dino_Puppet.ino
+++ Dino_Puppet.ino
@@ -48,108 +48,108 @@ Servo servos[SERVO_COUNT];
 int temp;
 
 void setup() {
-	
-  // Initialize motors
-  for(int i = 0; i < MOTOR_COUNT; i++) {
-    motors[i] = AFMS.getMotor(i+1);
-  }
 
-  // Initialize servos
-  for(int i = 0; i < SERVO_COUNT; i++) {
-    servos[i].attach(i+9);  // attaches the servos to pins 9 and 10
-  }
-  
-  Serial.begin(9600); // set up Serial library at 9600 bps
-  Serial.println("Dino puppet program - Life finds a way");
+	// Initialize motors
+	for(int i = 0; i < MOTOR_COUNT; i++) {
+		motors[i] = AFMS.getMotor(i+1);
+	}
 
-  AFMS.begin(); // create with the default frequency 1.6KHz
-  
-  for(int i = 0; i < MOTOR_COUNT; i++) {
-    motors[i]->setSpeed(128); // Set the speed to start, from 0 (off) to 255 (max speed)
-    motors[i]->run(FORWARD);
-    motors[i]->run(RELEASE); // turn on motor
-  }
+	// Initialize servos
+	for(int i = 0; i < SERVO_COUNT; i++) {
+		servos[i].attach(i+9);	// attaches the servos to pins 9 and 10
+	}
+
+	Serial.begin(9600); // set up Serial library at 9600 bps
+	Serial.println("Dino puppet program - Life finds a way");
+
+	AFMS.begin(); // create with the default frequency 1.6KHz
+
+	for(int i = 0; i < MOTOR_COUNT; i++) {
+		motors[i]->setSpeed(128); // Set the speed to start, from 0 (off) to 255 (max speed)
+		motors[i]->run(FORWARD);
+		motors[i]->run(RELEASE); // turn on motor
+	}
 }
 
 void loop() {
-	
+
 	//Serial.print("Running loop");
 
-  if (Serial.available() > 0) {
-    byte inByte = Serial.read();
+	if (Serial.available() > 0) {
+		byte inByte = Serial.read();
 		if(DEBUG) Serial.print(inByte);
-    
-    switch (inByte) {
-    
-    case UP:
-    motors[2]->run(FORWARD);
-    break;
-    
-    case DOWN: 
-    motors[2]->run(BACKWARD);
-    break;
-		
-		case VERTSTOP: 
-    motors[2]->run(RELEASE);
-    break;
-    
-    case LEFT: 
-    motors[1]->run(FORWARD);
-    break;
-    
-    case RIGHT: 
-    motors[1]->run(BACKWARD);
-    break;
-		
-		case HORIZSTOP: 
-    motors[1]->run(RELEASE);
-    break;
-		
-		case UP2:
-    motors[0]->run(BACKWARD);
-    break;
-    
-    case DOWN2: 
-    motors[0]->run(FORWARD);
-    break;
-		
-		case VERTSTOP2: 
-    motors[0]->run(RELEASE);
-    break;
 
-    case MOUTH:
+		switch (inByte) {
+
+		case UP:
+		motors[2]->run(FORWARD);
+		break;
+
+		case DOWN:
+		motors[2]->run(BACKWARD);
+		break;
+
+		case VERTSTOP:
+		motors[2]->run(RELEASE);
+		break;
+
+		case LEFT:
+		motors[1]->run(FORWARD);
+		break;
+
+		case RIGHT:
+		motors[1]->run(BACKWARD);
+		break;
+
+		case HORIZSTOP:
+		motors[1]->run(RELEASE);
+		break;
+
+		case UP2:
+		motors[0]->run(BACKWARD);
+		break;
+
+		case DOWN2:
+		motors[0]->run(FORWARD);
+		break;
+
+		case VERTSTOP2:
+		motors[0]->run(RELEASE);
+		break;
+
+		case MOUTH:
 		servos[0].write(180);
 		delay(500);
 		servos[0].write(0);
-    break;
+		break;
 
-    case EYES:
+		case EYES:
 		temp = servos[1].read();
-    servos[1].write(90);
+		servos[1].write(90);
 		delay(200);
 		servos[1].write(temp);
-    break;
-		
+		break;
+
 		case EYEstart:
 		while(Serial.available() <= 0) {} // Wait for the next byte to arrive
 		inByte = Serial.read();
 		if(DEBUG) Serial.println((int)inByte);
 		servos[1].write(inByte);
 		break;
-		
+
 		case MOUTHstart:
 		while(Serial.available() <= 0) {} // Wait for the next byte to arrive
 		inByte = Serial.read();
 		if(DEBUG) Serial.println((int)inByte);
 		servos[0].write(inByte);
 		break;
-    
-    default:
-    break;
-    
-    }
-  }
-	
+
+		default:
+		break;
+
+		}
+	}
+
 	//delay(1000);
 
 }
